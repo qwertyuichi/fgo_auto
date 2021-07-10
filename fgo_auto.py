1950c224ac7e353b9f59fb41b7d2e8043854e9a6
qwertyuichi <mmyuichi@gmail.com>
2021-07-10 22:31:26 +0900

    ウィンドウサイズの変更

--- fgo_auto.py
+++ fgo_auto.py
@@ -13,9 +13,9 @@ import matplotlib.pyplot as plt
 
 # キャプチャの設定
 WINDOW_NAME = "rpiplay"
-WINDOW_ID = "0x1e00002"
-WINDOW_WIDTH = 2532  # 1920
-WINDOW_HEIGHT = 1170  # 1080
+WINDOW_ID = "0x3a00002"
+WINDOW_WIDTH = 1728#2532
+WINDOW_HEIGHT = 798#1170
 IMAGE_WIDTH = 1823  # 960
 IMAGE_HEIGHT = 842  # 540
 
@@ -124,8 +124,8 @@ class ScreenCapture(threading.Thread):
 
         # キャプチャの初期設定
         self.video_source = cv2.VideoCapture(
-            # f"ximagesrc xname={WINDOW_NAME} ! videoconvert ! videoscale ! video/x-raw,width={WINDOW_WIDTH},height={WINDOW_HEIGHT} ! appsink"
-            f"ximagesrc xid={WINDOW_ID} ! videoconvert ! videoscale ! video/x-raw,width={WINDOW_WIDTH},height={WINDOW_HEIGHT} ! appsink"
+            f"ximagesrc xname={WINDOW_NAME} ! videoconvert ! videoscale ! video/x-raw,width={WINDOW_WIDTH},height={WINDOW_HEIGHT} ! appsink"
+            #f"ximagesrc xid={WINDOW_ID} ! videoconvert ! videoscale ! video/x-raw,width={WINDOW_WIDTH},height={WINDOW_HEIGHT} ! appsink"
         )
 
     def stop(self):
@@ -138,15 +138,10 @@ class ScreenCapture(threading.Thread):
             ret, image_original = self.video_source.read()
 
             if ret:
-                # w = 100
-                # self.image_color = image_original[:, w : 2532 - w]
                 # キャプチャした画像をリサイズして更新
                 self.image_color = cv2.resize(
                     image_original, (IMAGE_WIDTH, IMAGE_HEIGHT)
                 )
-                # self.image_color = cv2.resize(
-                #    self.image_color, (IMAGE_WIDTH, IMAGE_HEIGHT)
-                # )
 
                 # キャプチャした画像を表示
                 cv2.imshow("fgo_auto", self.image_color)
